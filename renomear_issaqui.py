d3d432f485a141c974236362b126a87e8c79f0e6
pfpimenta <pedro.pimenta@creatis.insa-lyon.fr>
2019-06-07 16:11:54 +0200

    python script test

---
+++ renomear_issaqui.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# script to ... ? escrever algo aqui
+# Pedro Foletto Pimenta, june-2019
+
+print("...initializing interface")
+# TODO
+print("...saving simulation parameters")
+# TODO
+print("...starting simulation")
+# TODO
+print("...simulation finished")
+# TODO
+print("..recovering results")
+# TODO
