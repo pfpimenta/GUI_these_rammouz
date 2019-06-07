2bd75082792f11ec7119402d6e0f87c61f80723f
pfpimenta <pedro.pimenta@creatis.insa-lyon.fr>
2019-06-07 16:36:55 +0200

    import tkinter

--- renomear_issaqui.py
+++ renomear_issaqui.py
@@ -3,9 +3,26 @@
 
 # script to ... ? escrever algo aqui
 # Pedro Foletto Pimenta, june-2019
+###
+
+#import tkinter
+from tkinter import *
+
+###
 
 print("...initializing interface")
 # TODO
+
+#from tkinter import *
+window = Tk()
+window.title("Welcome to LikeGeeks app")
+window.geometry('350x200')
+lbl = Label(window, text="Hello")
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Click Me")
+btn.grid(column=1, row=0)
+window.mainloop()
+
 print("...saving simulation parameters")
 # TODO
 print("...starting simulation")
