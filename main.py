3ad8f9e0cfc099fd45022f90478000f62f3121bf
Pedro Foletto Pimenta <pedrofolettopimenta@gmail.com>
2019-06-15 21:05:25 +0200

    two windows, entries

--- main.py
+++ main.py
@@ -7,21 +7,16 @@
 
 #import tkinter
 from tkinter import *
+from starting_window import *
+from composants_window import *
 
 ###
 
 print("...initializing interface")
 # TODO
-
-#from tkinter import *
-window = Tk()
-window.title("Welcome to LikeGeeks app")
-window.geometry('350x200')
-lbl = Label(window, text="Hello")
-lbl.grid(column=0, row=0)
-btn = Button(window, text="Click Me")
-btn.grid(column=1, row=0)
-window.mainloop()
+coco = Starting_window()
+caca = Composants_window()
+#cucu = Probleme_window()
 
 print("...saving simulation parameters")
 # TODO
